514e00b973665dbfe996defa0319e327b815937a
Nicholasdarosa <nicholas.darosa@hotmail.com>
2022-06-21 17:29:51 -0300

    Api-livraria

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
+})
+export class AppModule {}
---
+++ src/livros.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Livro } from './livro.model';
+import { LivrosService } from "./livros.service";
+
+@Controller('livros')
+export class LivrosController {
+    constructor(private livrosService: LivrosService) {
+
+    }
+
+    @Get()
+    async obterTodos(): Promise<Livro[]> {
+        return this.livrosService.obterTodos();
+    }
+
+    @Get(':id')
+    async obterUm(@Param() params): Promise<Livro> {
+        return this.livrosService.obterUm(params.id);
+    }
+
+    @Post()
+    async criar(@Body() livro: Livro) {
+        this.livrosService.criar(livro);
+    }
+
+    @Put()
+    async alterar(@Body() livro: Livro): Promise<[number, Livro[]]> {
+        return this.livrosService.alterar(livro);
+    }
+
+    @Delete(':id')
+    async apagar(@Param() params) {
+        this.livrosService.apagar(params.id);
+    }
+}
